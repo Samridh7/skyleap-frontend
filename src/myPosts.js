ce89f05d056def77b8850034f32ec2d34813c699
Samridh7 <88233183+Samridh7@users.noreply.github.com>
2023-08-07 19:31:04 +0530

    another commit

--- src/myPosts.js
+++ src/myPosts.js
@@ -3,10 +3,11 @@ import { useEffect, useState } from "react";
 import MyPostsLists from "./myPostsLists";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 const MyPosts = () => {
-    const [posts,setPosts] = useState(null)
+    const [posts,setPosts] = useState([])
     const userId = JSON.parse(localStorage.user).id;
     useEffect(() => {
         axios.get(`https://skyleap-app-09b1b3263d88.herokuapp.com/?userId=${userId}`).then((response) => {
+            // console.log(response.data);
             setPosts(response.data);
         }).catch((e) => {
             console.log(e);
@@ -15,7 +16,7 @@ const MyPosts = () => {
     return ( 
         <div id="content" className="myPosts">
             <h1>My Posts</h1>
-            {posts && <MyPostsLists posts={posts}></MyPostsLists>}
+            {posts.length > 0 ? <MyPostsLists posts={posts}></MyPostsLists> : <div className="no-posts">No posts yet</div>}
         </div>
      );
 }
